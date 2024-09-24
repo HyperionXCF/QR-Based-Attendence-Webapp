06f4f1d9bd3128b9ec7cc094f6cbfd8120aa9a01
Yash Sonalekar <67069340+HyperionXCF@users.noreply.github.com>
2024-09-24 21:49:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,18 @@
 import streamlit as st
+import pandas as pd
+import numpy as np 
 
 
 st.title("QR based Attendence Application")
 st.write("This App leverages Computer Vision & Python Libraries.")
 st.write("Made By - Team PyClub")
+
+df = pd.Dataframe({
+  'first column': [1,2,3,4],
+'second column':[10,20,30,40]
+})
+
+option = st.selectbox()
 
 
 

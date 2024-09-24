2277a6f0f7daf61c9e29d7ed6a9053db321af4ca
Yash Sonalekar <67069340+HyperionXCF@users.noreply.github.com>
2024-09-24 21:30:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,4 @@
 import streamlit as st
 
 st.title("QR based Attendence Application")
-st.write(
-    "This Project is completely made from scratch."
-    "This Project leverages Computer Vision to Scan the QR"
-    "and Some Python Dependencies to Process and Visualise the processed data."
-)
+

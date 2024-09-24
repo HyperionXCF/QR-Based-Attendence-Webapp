9b7f0b0430c3e8cb83056b19d0bb6d2a3c361308
Yash Sonalekar <67069340+HyperionXCF@users.noreply.github.com>
2024-09-24 21:45:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ st.title("QR based Attendence Application")
 st.write("This App leverages Computer Vision & Python Libraries.")
 st.write("Made By - Team PyClub")
 
-St.text("this is list of all the syntax that will be used for the project -")
 
 
 

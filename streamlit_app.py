43d12cfb1a499f0cf3b3cf6c05681e106f7ab9cf
Yash Sonalekar <67069340+HyperionXCF@users.noreply.github.com>
2024-09-24 21:44:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 
+
 st.title("QR based Attendence Application")
 st.write("This App leverages Computer Vision & Python Libraries.")
-
-
-
-
-
-
-
 st.write("Made By - Team PyClub")
+
+St.text("this is list of all the syntax that will be used for the project -")
+
+
 
